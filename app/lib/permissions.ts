97c62c9781c21373e00308e5548ba1b70c34070a
papkaandry <papkaandry@gmail.com>
2026-01-19 16:34:37 -0800

    Update permissions.ts

--- app/lib/permissions.ts
+++ app/lib/permissions.ts
@@ -1,8 +1,16 @@
-import { Role } from './users';
+export type Role = 'admin' | 'manager1' | 'manager2' | 'master';
 
 export const permissions: Record<Role, { accessSettings: boolean }> = {
-  admin: { accessSettings: true },
-  manager1: { accessSettings: false },
-  manager2: { accessSettings: false },
-  master: { accessSettings: false },
+  admin: {
+    accessSettings: true,
+  },
+  manager1: {
+    accessSettings: false,
+  },
+  manager2: {
+    accessSettings: false,
+  },
+  master: {
+    accessSettings: false,
+  },
 };
