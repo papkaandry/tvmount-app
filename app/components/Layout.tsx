79c92068d1709aa134b364cbc9fad27fbf074499
papkaandry <papkaandry@gmail.com>
2026-01-19 19:01:37 -0800

    Remove Sidebar component from Layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,14 +1,9 @@
 'use client';
 
-import Sidebar from './Sidebar';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <main style={{ padding: 24, flex: 1 }}>
-        {children}
-      </main>
-    </div>
+    <main style={{ padding: 24 }}>
+      {children}
+    </main>
   );
 }
